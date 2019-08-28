81a44a110c835718b715e72c376840eee879cb21
SeleSt <34377375+Yuri-Goncharik@users.noreply.github.com>
2019-08-28 17:42:54 +0300

    Second part of site

---
+++ js/galery.js
@@ -0,0 +1,13 @@
+function modal(t)
+{
+	alert("mda");
+	 // var modall = new Modal();
+  //    modall.setContent('<div id="m1" class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" aria-hidden="true">
+  //   <div class="modal-dialog modal-lg">
+  //     <div class="modal-content">
+  //       <img src="images/slider/1b.jpg" width="100%; ">
+  //     </div>
+  //   </div>
+  // </div>');
+
+}
